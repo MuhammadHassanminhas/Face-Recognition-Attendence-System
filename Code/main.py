6d809f469500ac359a4629b135d84cee98a08670
MuhammadHassanminhas <muhammadhassan1762005@gamil.com>
2024-09-17 14:42:36 +0500

    data_collection

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,5 @@
+# import all_lib
 import cv2
-
 # Load the pre-trained Haar Cascade classifier for face detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -16,7 +16,7 @@ while True:
     # Detect faces in the grayscale frame
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Draw bounding boxes around the detected faces
+  # Draw bounding boxes around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
@@ -26,7 +26,6 @@ while True:
     # Break the loop if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 # Release the video capture object and close all OpenCV windows
-cap.release()
+cap.release()  
 cv2.destroyAllWindows()
---
+++ Code/model.py
@@ -0,0 +1,8 @@
+import torch
+
+class Image_Classification(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+obj = Image_Classification()
